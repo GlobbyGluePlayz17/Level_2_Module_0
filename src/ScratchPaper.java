f6d94676250fabd0486d1b3354d973a50a1ed5fe
GlobbyGluePlayz17 <league@WTS14.attlocal.net>
2018-06-09 15:54:45 -0700

    ArrayCheeetSheet(IP)

---
+++ src/ScratchPaper.java
@@ -0,0 +1,29 @@
+
+public class ScratchPaper {
+	// arrays: variable that holds multiple values
+	// String[] animals = ["koala", "banane"];
+	// syso(animals[0]);
+	// to set later: String[] animals = new int [2]
+	// when printing specific number value in array, must -1 cuz first starts from 0
+	// can print when all values are not set
+	// to print the last value of the array, you must do 'syso(names[names.length -
+	// 1])'; if you had 3 string in an array, then the length would be 3 but the
+	// last index is 2 cuz 0, 1, 2
+
+	// arraylists: - stores objects w/o wrapper classes
+	// ArrayList<String> fruit = new ArrayList<String>();
+	// wrapper class - int = Integer, char = Character, boolean = Boolean, long =
+	// Long
+	// print all values in arraylist -
+	// standard for loop: for(int i = 0; i< frui.size(); i++){
+	// }syso(fruit.get(i));
+	// }
+	// or(enhanced for loop or for-each lop): for(String s:fruit){
+	// syso(s);
+	// }
+
+	// difference of arrays and arraylists
+	// array - cannot change number of values
+	// arraylist - can change value
+
+}
--- src/intro_to_array_lists/ScratchPaper.java
+++
@@ -1,8 +0,0 @@
-package intro_to_array_lists;
-
-public class ScratchPaper {
-	//arrays: variable that holds multiple values
-		//String[] animals = ["koala", "banane"];
-		//syso(animals[0]);
-		//to set later: String[] animals = new int [2]
-}
